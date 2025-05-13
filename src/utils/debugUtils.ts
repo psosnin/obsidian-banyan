b61b0e9bed65afa3ef3f502b55bb21cebc282c67
Ted <2084939306@qq.com>
2025-05-13 12:21:14 +0800

    misc: 添加注释，应对obsidian review

--- src/utils/debugUtils.ts
+++ src/utils/debugUtils.ts
@@ -1,5 +1,5 @@
 export const snapshot = (el: Element): any => ({
-    html: el.outerHTML,
+    html: el.outerHTML, // /skip for debug use only
     rect: el.getBoundingClientRect(),
     children: Array.from(el.children).map(child => snapshot(child))
 });

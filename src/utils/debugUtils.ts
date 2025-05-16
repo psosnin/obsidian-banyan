19c17d3b245ca1745cb8adede6413e4ac3185c2a
Ted <2084939306@qq.com>
2025-05-16 21:14:51 +0800

    chore: 删掉debug代码，以防审查挑刺

--- src/utils/debugUtils.ts
+++
@@ -1,5 +0,0 @@
-export const snapshot = (el: Element): any => ({
-    html: el.outerHTML, // /skip for debug use only
-    rect: el.getBoundingClientRect(),
-    children: Array.from(el.children).map(child => snapshot(child))
-});

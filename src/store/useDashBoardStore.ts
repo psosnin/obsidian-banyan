5d325d881984d707860d1be99ecd7baf88c31d2d
Ted <2084939306@qq.com>
2025-06-27 11:42:08 +0800

    opt: 搜索 - 去除markdown格式符合 - 新增高亮和着重

--- src/store/useDashBoardStore.ts
+++ src/store/useDashBoardStore.ts
@@ -98,10 +98,12 @@ export const useDashBoardStore: StateCreator<CombineState, [], [], DashBoardStat
 
 const stripMarkdown = (mdStr: string) => {
     return mdStr
-      .replace(/(\*\*|__)(.*?)\1/g, '$2')  // 加粗
-      .replace(/(\*|_)(.*?)\1/g, '$2')     // 斜体
-      .replace(/~~(.*?)~~/g, '$1')         // 删除线
-      .replace(/`(.*?)`/g, '$1')           // 行内代码
-      .replace(/!?$$(.*?)$$$.*?$/g, '$1') // 链接和图片
-      .replace(/^#+\s+/gm, '');            // 标题
+      .replace(/(\*\*\*|__)(.*?)\1/g, '$2')  // 着重
+      .replace(/(\*\*|__)(.*?)\1/g, '$2')    // 加粗
+      .replace(/(\*|_)(.*?)\1/g, '$2')       // 斜体
+      .replace(/(==|__)(.*?)\1/g, '$2')      // 高亮
+      .replace(/~~(.*?)~~/g, '$1')           // 删除线
+      .replace(/`(.*?)`/g, '$1')             // 行内代码
+      .replace(/!?$$(.*?)$$$.*?$/g, '$1')    // 链接和图片
+      .replace(/^#+\s+/gm, '');              // 标题
   }
